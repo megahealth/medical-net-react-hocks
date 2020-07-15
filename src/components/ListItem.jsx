3afe5d2f30aef6ad5d96fcacd975b9c2fcc001ab
jichao <iamjichao@163.com>
2020-07-15 18:32:35 +0800

    init

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div>list item</div>
+    );
+  }
+}
+ 
+export default ListItem;
